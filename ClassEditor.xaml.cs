774be50a72d16807c3b7972873b83ce84cd0427b
bertwaver <2797920819@qq.com>
2024-02-03 05:22:41 +0800

    ClassEditor的进一步设计

--- ClassEditor.xaml.cs
+++ ClassEditor.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Controls.Primitives;
 
 namespace signup
 {
@@ -10,6 +11,28 @@ namespace signup
         public ClassEditor()
         {
             InitializeComponent();
+        }
+
+        private void LayCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //"直接编辑本机方案"按钮被按下事件
+        private void Btn_EditSelf_Click(object sender, RoutedEventArgs e)
+        {
+            Editor_TabContrl.SelectedIndex = 1;
+        }
+
+        private void Btn2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        
+        //滚动条进行滚动时执行的操作
+        private void Editor_ScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+
         }
     }
 }
